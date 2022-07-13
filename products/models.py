982ed6adc66f379a325421b41e23c8808edd4a4c
programista10kpln <programista10kpln@proton.me>
2022-07-13 16:02:29 +0200

    added db to admin, created website

--- products/models.py
+++ products/models.py
@@ -7,4 +7,10 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     stock = models.IntegerField()
-    image_url = models.CharField(max_length=2083)
+    image_url = models.CharField(max_length=2048)
+
+
+class Offer(models.Model):
+    code = models.CharField(max_length=7)
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
